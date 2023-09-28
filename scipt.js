6d9e08d3a2d64a5610a344118c77ca61b378ab14
wileland <wileland7@gmail.com>
2023-09-27 22:35:27 -0500

    Correct 12 hour display achieved. I shall continue on my own because this is so rewarding! But am submitting due to time.

--- scipt.js
+++ scipt.js
@@ -1,4 +1,3 @@
-scipt.js
 $(function () {
     // Function to get the current date and display it in the header
     function displayCurrentDate() {
@@ -6,64 +5,39 @@ $(function () {
         $("#currentDay").text(currentDate);
     }
 
-    // Function to check and update the time-block classes (past, present, future)
-    function updateHourlyBlocks() {
+    // Function to update the day column with color-coded hours
+    function updateDayColumn() {
         var currentHour = dayjs().hour();
+        var dayColumn = $(".day-column");
+        
+        // Clear the day column
+        dayColumn.empty();
 
-        $(".time-block").each(function () {
-            var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        // Generate the 12-hour cycle
+        for (var i = 1; i <= 12; i++) {
+            var hourDisplay = i <= 9 ? "0" + i : i;
+            var ampm = currentHour >= 12 ? "PM" : "AM";
+            var hour = currentHour >= 12 ? currentHour - 12 : currentHour;
 
-            if (blockHour < currentHour) {
-                $(this).removeClass("present future").addClass("past");
-            } else if (blockHour === currentHour) {
-                $(this).removeClass("past future").addClass("present");
+            var hourBlock = $("<div>")
+                .addClass("hour-block")
+                .text(hourDisplay + " " + ampm);
+
+            // Add different classes based on the current hour
+            if (i === hour) {
+                hourBlock.addClass("current-hour");
+            } else if (i < hour) {
+                hourBlock.addClass("past-hour");
             } else {
-                $(this).removeClass("past present").addClass("future");
+                hourBlock.addClass("future-hour");
             }
-        });
+
+            // Append the hour block to the day column
+            dayColumn.append(hourBlock);
+        }
     }
-
-    // Function to load saved events from local storage
-    function loadEvents() {
-        $(".time-block").each(function () {
-            var blockId = $(this).attr("id");
-            var savedEvent = localStorage.getItem(blockId);
-
-            if (savedEvent) {
-                $(this).find(".description").val(savedEvent);
-            }
-        });
-    }
-
-    // Function to save events to local storage
-    $(".container-fluid").on("click", ".saveBtn", function () {
-        var blockId = $(this).closest(".time-block").attr("id");
-        var eventText = $(this).siblings(".description").val();
-
-        localStorage.setItem(blockId, eventText);
-    });
 
     // Call functions to initialize the scheduler
     displayCurrentDate();
-    loadEvents();
-    // Generate time blocks dynamically
-    for (var i = 9; i <= 17; i++) {
-        var ampm = i < 12 ? "AM" : "PM";
-        var displayHour = i <= 12 ? i : i - 12;
-
-        var timeBlock = `
-            <div id="hour-${i}" class="row time-block">
-                <div class="col-2 col-md-1 hour text-center py-3">${displayHour}${ampm}</div>
-                <textarea class="col-8 col-md-10 description" rows="3"></textarea>
-                <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-                    <i class="fas fa-save" aria-hidden="true"></i>
-                </button>
-            </div>
-        `;
-
-        $(".container-fluid").append(timeBlock);
-    }
-
-    // Set an interval to update the time-block classes every minute
-    setInterval(updateHourlyBlocks, 60000);
+    updateDayColumn();
 });
